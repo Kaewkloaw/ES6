77abebf013753b56d9409736f5061aebc3331b01
Punchaya Chancharoen <149894554+Kaewkloaw@users.noreply.github.com>
2026-01-06 04:32:55 +0700

    Update and rename ES6/loopArray.js to loopArray.js

--- loopArray.js
+++ loopArray.js
@@ -2,5 +2,3 @@
 // -for loop : loops through a block of code a number of times
 // -forEach : calls a function for each element in the array
 // -Forof : loops through the values of an iterable object
-
-//2:25:00
